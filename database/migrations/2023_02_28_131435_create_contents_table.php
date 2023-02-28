a7d594d70bbad62d480f97e8362ec3114b500380
MizouziE <s.anglin@live.co.uk>
2023-02-28 15:25:23 +0100

    create Content model, factory, controller

---
+++ database/migrations/2023_02_28_131435_create_contents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\ContentTemplate;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->longText('body')->nullable();
+            $table->foreignIdFor(ContentTemplate::class)->onUpdate('cascade')->onDelete('cascade')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
