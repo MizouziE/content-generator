c1fcec9f38ce64d0aa63ff9f5eff9c07393a229d
MizouziE <s.anglin@live.co.uk>
2023-03-01 12:41:58 +0100

    add back button

--- resources/js/Pages/ContentTemplates/Show.vue
+++ resources/js/Pages/ContentTemplates/Show.vue
@@ -12,14 +12,14 @@ defineProps({ template: Object });
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Content Template
             </h2>
-            <Link :href="route('contentTemplates')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-            Back to List
-            </Link>
         </template>
-
+        
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-12">
+                <Link :href="route('contentTemplates')" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                Back to List
+                </Link>
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-12 mt-4">
 
                     <div class="" v-for="(info, index) in template">
                         <span class="font-bold">{{ index }}:</span> {{ info }}
