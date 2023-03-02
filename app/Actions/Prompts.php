4889b62a3d3fbe89c2e36831790d15d70da6144a
MizouziE <s.anglin@live.co.uk>
2023-03-02 16:33:32 +0100

    make preg match case insensitive

--- app/Actions/Prompts.php
+++ app/Actions/Prompts.php
@@ -7,7 +7,7 @@ class Prompts
     public static function prepare(array $patterns, array $replacements, array $subjects): array
     {
         foreach ($patterns as $i => $pattern) {
-            $patterns[$i] = '/{{\s?' . $pattern . '\s?}}/';
+            $patterns[$i] = '/{{\s?' . $pattern . '\s?}}/i';
         }
 
         return preg_replace($patterns, $replacements, $subjects);
